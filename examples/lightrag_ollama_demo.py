a8af1f5b1ed046150dcee8e8b600fd617add55ca
samia-101245 <2019-3-60-116@std.ewubd.edu>
2025-02-28 19:57:41 +0600

    UPDATED

--- examples/lightrag_ollama_demo.py
+++ examples/lightrag_ollama_demo.py
@@ -36,7 +36,7 @@ rag = LightRAG(
 # with open("./book.txt", "r", encoding="utf-8") as f:
 #     rag.insert(f.read())
 
-file_path = 'book_ollama/output.csv'
+file_path = 'book_ollama/output(1).csv'
 text_content = textract.process(file_path)
 
 rag.insert(text_content.decode('utf-8'))
